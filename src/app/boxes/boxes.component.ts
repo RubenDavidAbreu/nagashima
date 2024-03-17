d41ca94bb6153c91148de53b751083114cdb31c7
Jeremie-PARANT <parantjeremie@gmail.com>
2024-03-17 16:32:18 +0100

    systeme de reservation

--- src/app/boxes/boxes.component.ts
+++ src/app/boxes/boxes.component.ts
@@ -1,6 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { CamionService } from '../camion.service';
 
+export class CommandeItem {
+  constructor(public box: any, public quantity: number) {}
+}
+
+export class Commande {
+  items: CommandeItem[] = [];
+  tooManyBoxesReached: boolean = false; // Déplacez la propriété ici
+
+  addItem(box: any) {
+    if (this.getTotalQuantity() < 10) {
+      const existingItem = this.items.find(item => item.box === box);
+      if (existingItem) {
+        existingItem.quantity += 1;
+      } else {
+        this.items.push(new CommandeItem(box, 1));
+      }
+    } else {
+      this.tooManyBoxesReached = true;
+    }
+  }
+
+  notTooMany() {
+    this.tooManyBoxesReached = false;
+  }
+
+  removeItem(box: any) {
+    const existingItem = this.items.find(item => item.box === box);
+    if (existingItem) {
+      if (existingItem.quantity > 1) {
+        existingItem.quantity -= 1; // Décrémenter la quantité
+      } else {
+        const index = this.items.indexOf(existingItem);
+        if (index !== -1) {
+          this.items.splice(index, 1); // Supprimer complètement si la quantité est 1
+        }
+      }
+    }
+  }
+
+  getTotalPrice(): number {
+    return this.items.reduce((total, item) => total + (item.box.price * item.quantity), 0);
+  }
+
+  getTotalQuantity(): number {
+    return this.items.reduce((total, item) => total + item.quantity, 0);
+  }
+}
+
 @Component({
   selector: 'app-boxes',
   templateUrl: './boxes.component.html',
@@ -10,6 +58,7 @@ export class BoxesComponent implements OnInit {
   constructor(public camions: CamionService) {}
 
   boxes: any[] = [];
+  commande: Commande = new Commande();
 
   ngOnInit(): void {
     this.camions.camion().subscribe((data) => {
@@ -23,5 +72,13 @@ export class BoxesComponent implements OnInit {
       flavorClasses += `${flavor.name} `;
     }
     return flavorClasses.trim();
+  }
+
+  addToCommande(box: any) {
+    this.commande.addItem(box);
+  }
+
+  removeFromCommande(box: any) {
+    this.commande.removeItem(box);
   }
 }
