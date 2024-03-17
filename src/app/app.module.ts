46e77c8e5b46d98a8ccc2fdf22e0969173db05dc
Lemoil-Arno <119615707+Lemoil-Arno@users.noreply.github.com>
2024-03-17 16:31:55 +0100

    ajout des component (routing)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoxesComponent } from './boxes/boxes.component';
 import { HttpClientModule } from '@angular/common/http';
+import { AccueilComponent } from './accueil/accueil.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { DetailsComponent } from './details/details.component';
+import { PanierComponent } from './panier/panier.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoxesComponent
+    BoxesComponent,
+    AccueilComponent,
+    HeaderComponent,
+    FooterComponent,
+    DetailsComponent,
+    PanierComponent
   ],
   imports: [
     BrowserModule,
